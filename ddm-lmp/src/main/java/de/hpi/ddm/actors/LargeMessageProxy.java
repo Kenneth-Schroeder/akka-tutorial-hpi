0e2c585d25e3dbc5f9f31be5174c1e2bf69b24b7
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2020-05-11 14:36:48 +0200

    add comments for understanding

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -12,71 +12,74 @@ import lombok.NoArgsConstructor;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
+    public static final String DEFAULT_NAME = "largeMessageProxy";
 
-	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
-	public static Props props() {
-		return Props.create(LargeMessageProxy.class);
-	}
+    public static Props props() {
+        return Props.create(LargeMessageProxy.class);
+    }
 
-	////////////////////
-	// Actor Messages //
-	////////////////////
-	
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class LargeMessage<T> implements Serializable {
-		private static final long serialVersionUID = 2940665245810221108L;
-		private T message;
-		private ActorRef receiver;
-	}
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LargeMessage<T> implements Serializable { // INFO: this tells the proxy to send a large message and to who
+        private static final long serialVersionUID = 2940665245810221108L;
+        private T message;
+        private ActorRef receiver;
+    }
 
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
-		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private ActorRef sender;
-		private ActorRef receiver;
-	}
-	
-	/////////////////
-	// Actor State //
-	/////////////////
-	
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BytesMessage<T> implements Serializable {
+        private static final long serialVersionUID = 4057807743872319842L;
+        private T bytes;
+        private ActorRef sender;
+        private ActorRef receiver;
+    }
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
-	
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
+    /////////////////
+    // Actor State //
+    /////////////////
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(LargeMessage.class, this::handle)
+                .match(BytesMessage.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
 
-	private void handle(LargeMessage<?> message) {
-		ActorRef receiver = message.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
-	}
+    private void handle(LargeMessage<?> message) {
+        ActorRef receiver = message.getReceiver();
+        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME)); // INFO: lookup the receiving proxy through the path of the receiver
 
-	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-		message.getReceiver().tell(message.getBytes(), message.getSender());
-	}
+        // TODO
+        // This will definitely fail in a distributed setting if the serialized message is large!
+        // Solution options:
+        // 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
+        // 2. Serialize the object and send its bytes via Akka streaming.
+        // 3. Send the object via Akka's http client-server component.
+        // 4. Other ideas ...
+        
+        // INFO: currently trying to send the large message over .tell()
+        receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
+    }
+
+    private void handle(BytesMessage<?> message) { // INFO: receiving the large message 
+        // TODO
+        // Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+        message.getReceiver().tell(message.getBytes(), message.getSender());
+    }
 }
